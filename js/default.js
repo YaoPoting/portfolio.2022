2fa85dd412580ed8f9c3a9d4f15e6134a91fbf33
YaoPoting <104891902+YaoPoting@users.noreply.github.com>
2024-06-17 16:11:01 +0800

    nav

--- js/default.js
+++ js/default.js
@@ -7,17 +7,17 @@ new WOW().init();
 function scrollToWebsites() {
     const anchor = document.querySelector('.website');
     anchor.scrollIntoView({ behavior: 'smooth', block: 'start' });
-}
+};
 
 window.onscroll = function() {
     var myNav = document.querySelector('.myNav');
     var navPosition = myNav.getBoundingClientRect();
 
-    if (navPosition.top === 0) {
+    if (navPosition.top <= 0) {
         myNav.style.backgroundColor = "#FFB000";
         myNav.style.color = "#FFFFFF";
     } else {
         myNav.style.backgroundColor = "";
-        myNav.style.color = "#000000";
+        myNav.style.color = "";
     }
 };
