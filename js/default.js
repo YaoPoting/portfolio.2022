508e0b96dd6fdaf51fd049f723a2838764510263
YaoPoting <104891902+YaoPoting@users.noreply.github.com>
2024-06-17 16:34:20 +0800

    Update default.js

--- js/default.js
+++ js/default.js
@@ -9,15 +9,27 @@ function scrollToWebsites() {
     anchor.scrollIntoView({ behavior: 'smooth', block: 'start' });
 };
 
-window.onscroll = function() {
+window.onload = function() {
     var myNav = document.querySelector('.myNav');
-    var navPosition = myNav.getBoundingClientRect();
+    var ticking = false;
 
-    if (navPosition.top <= 0) {
-        myNav.style.backgroundColor = "#FFB000";
-        myNav.style.color = "#FFFFFF";
-    } else {
-        myNav.style.backgroundColor = "";
-        myNav.style.color = "";
+    function updateNav() {
+        var navPosition = myNav.getBoundingClientRect();
+
+        if (navPosition.top >= 0) {
+            myNav.style.backgroundColor = "#FFB000";
+            myNav.style.color = "#FFFFFF";
+        } else {
+            myNav.style.backgroundColor = "#FFFFFF"; // 指定一个非空的背景颜色
+            myNav.style.color = "#000000";
+        }
+        ticking = false;
     }
+
+    window.onscroll = function() {
+        if (!ticking) {
+            window.requestAnimationFrame(updateNav);
+            ticking = true;
+        }
+    };
 };
