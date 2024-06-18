c850133a2b822b7c9495c176f8cf7aff3e866934
YaoPoting <104891902+YaoPoting@users.noreply.github.com>
2024-06-18 13:37:20 +0800

    nav fixed

--- js/default.js
+++ js/default.js
@@ -9,15 +9,39 @@ function scrollToWebsites() {
     anchor.scrollIntoView({ behavior: 'smooth', block: 'start' });
 };
 
-window.onscroll = function() {
-    var myNav = document.querySelector('.myNav');
-    var navPosition = myNav.getBoundingClientRect();
+// window.onscroll = function() {
+//     var myNav = document.querySelector('.myNav');
+//     var navPosition = myNav.getBoundingClientRect();
 
-    if (navPosition.top <= 0) {
-        myNav.style.backgroundColor = "#FFB000";
-        myNav.style.color = "#FFFFFF";
+//     if (navPosition.top <= 0) {
+//         myNav.style.backgroundColor = "#FFB000";
+//         myNav.style.color = "#FFFFFF";
+//     } else {
+//         myNav.style.backgroundColor = "#FFFFFF";
+//         myNav.style.color = "#000000";
+//     }
+// };
+
+// window.addEventListener('scroll', function() {
+//     var navbar = document.querySelector('.myNav');
+//     var hero = document.querySelector('.hero');
+//     var heroHeight = hero.offsetHeight;
+
+//     if (window.scrollY > heroHeight) {
+//         navbar.classList.add('sticky');
+//     } else {
+//         navbar.classList.remove('sticky');
+//     }
+// });
+
+window.onscroll = function() {
+    var navbar = document.querySelector('.myNav');
+    var kv = document.querySelector('.kv');
+    var kvHeight = kv.offsetHeight;
+
+    if (window.scrollY > kvHeight) {
+        navbar.classList.add('sticky');
     } else {
-        myNav.style.backgroundColor = "#FFFFFF";
-        myNav.style.color = "#000000";
+        navbar.classList.remove('sticky');
     }
 };
