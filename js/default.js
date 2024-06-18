e59d142154b05adebb434a933081d142a62f1079
YaoPoting <104891902+YaoPoting@users.noreply.github.com>
2024-06-18 13:45:11 +0800

    nav bar

--- js/default.js
+++ js/default.js
@@ -9,32 +9,7 @@ function scrollToWebsites() {
     anchor.scrollIntoView({ behavior: 'smooth', block: 'start' });
 };
 
-// window.onscroll = function() {
-//     var myNav = document.querySelector('.myNav');
-//     var navPosition = myNav.getBoundingClientRect();
-
-//     if (navPosition.top <= 0) {
-//         myNav.style.backgroundColor = "#FFB000";
-//         myNav.style.color = "#FFFFFF";
-//     } else {
-//         myNav.style.backgroundColor = "#FFFFFF";
-//         myNav.style.color = "#000000";
-//     }
-// };
-
-// window.addEventListener('scroll', function() {
-//     var navbar = document.querySelector('.myNav');
-//     var hero = document.querySelector('.hero');
-//     var heroHeight = hero.offsetHeight;
-
-//     if (window.scrollY > heroHeight) {
-//         navbar.classList.add('sticky');
-//     } else {
-//         navbar.classList.remove('sticky');
-//     }
-// });
-
-window.onscroll = function() {
+window.addEventListener('scroll', function() {
     var navbar = document.querySelector('.myNav');
     var kv = document.querySelector('.kv');
     var kvHeight = kv.offsetHeight;
@@ -44,4 +19,4 @@ window.onscroll = function() {
     } else {
         navbar.classList.remove('sticky');
     }
-};
+});
